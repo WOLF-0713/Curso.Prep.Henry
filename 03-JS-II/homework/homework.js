88c503b80dd68daa3f6335fc24394193a8eda178
WOLF-0713 <luiscardenas920717@gmail.com>
2021-10-03 16:23:51 -0500

    04-JS-III/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -19,7 +19,7 @@ function mayoriaDeEdad(edad) {
     return "Allowed";
   } else {
     return "Not allowed"
-    }
+   }
   }
   
 function conection(status) {
@@ -33,8 +33,7 @@ function conection(status) {
   }
   else if(status === 2) {
     return "Away";
-  }
-  else {
+  } else {
     return "Offline";
   }
 }
@@ -46,14 +45,16 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  if(idioma === 'aleman') {
-    return 'Guten Tag!';
-  } else if (idioma === 'mandarin') {
-    return 'Ni Hao!';
-  } else if (idioma === 'ingles') {
-    return 'Hello!';
+  if(idioma === "aleman") {
+    return "Guten Tag!";
+  }
+  if(idioma === "mandarin") {
+    return "Ni Hao!";
+  }
+  if(idioma === "ingles") {
+    return "Hello!";
   } else {
-    return 'Hola!';
+    return "Hola!"
   }
 }
 
@@ -65,25 +66,29 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-  switch(color) {
-    case "blue":
-      return "This is blue";
-    case "red": 
-      return "This is red";
-    case "green":
-      return "This is green";
-    case "orange":
-      return "This is orange";
-    default: 
-    return "Color not found";
-  }
+switch (color) {
+case "blue":
+return "This is blue";
+case "red":
+return "This is red";
+case "green":
+return "This is green";
+case "orange":
+return "This is orange";
+default:
+  return "Color not found";
+}
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  return numero === 10 || numero === 5;
+ if (numero === 10 ||numero === 5) {
+   return true;
+} else {
+  return false;
+}
 }
 
 function estaEnRango(numero) {
@@ -101,7 +106,12 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  return numero % 1 === 0;
+  //return numero % 1 === 0;
+  if (numero === Math.floor (numero)) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -109,9 +119,19 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if(numero % 15 === 0) return 'fizzbuzz';
-  if(numero % 3 === 0) return 'fizz';
-  if(numero % 5 === 0) return 'buzz';
+  // if(numero % 15 === 0) return 'fizzbuzz';
+  // if(numero % 3 === 0) return 'fizz';
+  // if(numero % 5 === 0) return 'buzz';
+  // return numero;
+  if (numero % 3 === 0 && numero % 5 === 0) {
+  return "fizzbuzz"
+  }
+  if (numero % 3 === 0) {
+  return "fizz";
+  }
+  if (numero % 5 === 0) {
+  return "buzz"
+  }  
   return numero;
 }
 
@@ -122,19 +142,33 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
-  if(num1 < 0 || num2 < 0 || num3 < 0) {
+  // if(num1 < 0 || num2 < 0 || num3 < 0) {
+  //   return "Hay negativos";
+  // }
+  // else if(num1 === 0 || num2 === 0 || num3 === 0) {
+  //   return "Error";
+  // }
+  // else if(num1 > 0 && num1 > num2 && num1 > num3) {
+  //   return "Número 1 es mayor y positivo";
+  // }
+  // else if(num3 > num1 && num3 > num2) {
+  //   return num3 + 1;
+  // }
+  // else {
+  //   return false;
+  // }
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
     return "Hay negativos";
   }
-  else if(num1 === 0 || num2 === 0 || num3 === 0) {
-    return "Error";
+  if (num1 === 0 || num2 === 0 || num3 === 0 ) {
+    return "Error"
   }
-  else if(num1 > 0 && num1 > num2 && num1 > num3) {
-    return "Número 1 es mayor y positivo";
+  if (num1 > num2 && num1 > num3 && num1 > 0) {
+return "Número 1 es mayor y positivo";
   }
-  else if(num3 > num1 && num3 > num2) {
+  if (num3 > num1 && num3 > num2) { 
     return num3 + 1;
-  }
-  else {
+  } else {
     return false;
   }
 }
@@ -145,35 +179,59 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if( numero < 2) return false;
-  if(numero === 2) return true;
-  for(var i = 2; i < numero; i++) {
-    if(numero % i === 0) {
-      return false;
-    }
-  }
+//   if( numero < 2) return false;
+//   if(numero === 2) return true;
+//   for(var i = 2; i < numero; i++) {
+//     if(numero % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+if (numero < 2) {
+  return false;
+} 
+if (numero === 2) {
   return true;
+}
+for (var i = 2; i < numero; i++) {
+  if(numero % i === 0) 
+    return false;
+}  
+    return true;
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if(valor === true){
-    return "Soy verdadero"
-  }
-  return "Soy falso"
+//   if(valor === true){
+//     return "Soy verdadero"
+//   }
+//   return "Soy falso"
+// }
+if (valor === true) {
+  return "Soy verdadero"
 }
-
+if (valor === false) {
+return "Soy falso"
+}
+}
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  let arrayTablaDel6 = []
-  for (let i = 0; i < 11; i++) {
-        arrayTablaDel6.push(6 * i)
-  }
-  return arrayTablaDel6
+//   let arrayTablaDel6 = []
+//   for (let i = 0; i < 11; i++) {
+//         arrayTablaDel6.push(6 * i)
+//   }
+//   return arrayTablaDel6
+// }
+var result = [];
+for (var i = 0; i < 11; i++) {
+  result.push (6 * i);
+}
+return result;
 }
 
 function tieneTresDigitos(numero){
